92ba9a241f7c6cb4c6ed00beee19d3ff58475834
Bruno Ferme Gasparin <bfgasparin@MacBook-Pro-de-Bruno.local>
2014-04-21 01:34:29 -0300

    Added teacher lab1 example

---
+++ src/mainExemplo.m
@@ -0,0 +1,13 @@
+P = readText('Resources/files/NotesUndergound.txt');
+
+C = monoalfabeticaE(P,'knmporqtsvuxwzybadcfeghjil');
+%saveText(C,'crimepunishment_Cifrado.txt');
+[rateMono character] = countCharacters(C);
+
+    
+plot([1:26],rateMono);
+legend('Monoalfabetica');
+title('Frequencia de Letras em Textos Cifrados');
+xlabel('letras');
+ylabel('percentagem de ocorrencia');
+print -dpng 'lab1.png'
