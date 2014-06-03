0741ef969a7ca28d67813ea8c5fd5bb5aef2c81d
Victor Ferme Gasparin <victor.f.gasparin@gmail.com>
2014-06-03 17:10:07 -0300

    Cirrgindo Shift

--- src/cipher/feistel.m
+++ src/cipher/feistel.m
@@ -12,15 +12,16 @@ KLength = length(K);
 for i=1:rodadas
 
 	bit = mod(rodadas, KLength) + 1;
-	
-	subK(bit) = ~subK(bit); %inverte um dos bits da chave K
+
+	%MultiK com Mod	
+	%subK(bit) = ~subK(bit); %inverte um dos bits da chave K
 
 	%MultiK com Shift
-	%subKTemp = [subKTemp(lastBit) subKTemp(1:lastBit-1)];
-	%for Kindex=1:cols
-	%	indexTemp = cols*Kindex - cols + 1;
-	%	subK(Kindex:cols) = subKTemp(indexTemp:cols*Kindex);
-	%end
+	subKTemp = [subKTemp(lastBit) subKTemp(1:lastBit-1)];
+	for Kindex=1:cols
+		indexTemp = cols*Kindex - cols + 1;
+		subK(indexTemp:cols*Kindex) = subKTemp(indexTemp:cols*Kindex);
+	end
 
 	CR = C(blocoSize+1:2*blocoSize);
 	
