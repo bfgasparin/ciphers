059aa23a1145bd3e7c0eaed58dbb55a707541ec8
Victor Ferme Gasparin <victor.f.gasparin@gmail.com>
2014-07-28 01:37:02 -0300

    Corrigindo funcao de exponencicao. O binario precisa ser invertido, entao invertemos o loop

--- src/exp5/pegar_mod.m
+++ src/exp5/pegar_mod.m
@@ -1,18 +1,18 @@
 
 function  res = pegar_mod(base, e, m)
-    b=poliGet_binarioInvertido(e, 2, 128);
+    b=dec2bin(e);
     k=size(b,2);
     c=0;
     f=1;
-    i=k;
-    while i>0
+    i=1;
+    while i<=k
        c = 2 * c;
        f=mod((f*f),m);
-       if(b(i)==1)
+       if(b(i)=='1')
           c=c+1;
           f = mod((f*base), m);
        end 
-       i=i-1;
+       i=i+1;
     end
     res=f;
 end
--- src/exp5/poliGet_binarioInvertido.m
+++
@@ -1,7 +0,0 @@
-function polinomial = poliGet_binarioInvertido(decimal, p, n)
-
-polinomial = zeros(1,n);
-for i=1:n
-    polinomial(i) = mod (decimal,p);
-    decimal = floor(decimal/p);
-end
