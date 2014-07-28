490b2a18ed0ff940b09ebe8f28653c5f6379f866
Victor Ferme Gasparin <victor.f.gasparin@gmail.com>
2014-07-28 01:24:33 -0300

    Corrigindo funcao de exponencicao. O binario precisa ser gerado invertido

--- src/exp5/pegar_mod.m
+++ src/exp5/pegar_mod.m
@@ -1,6 +1,6 @@
 
 function  res = pegar_mod(base, e, m)
-    b=dec2bin(e);
+    b=poliGet_binarioInvertido(e, 2, 128);
     k=size(b,2);
     c=0;
     f=1;
@@ -8,12 +8,12 @@ function  res = pegar_mod(base, e, m)
     while i>0
        c = 2 * c;
        f=mod((f*f),m);
-       if(b(i)=='1')
+       if(b(i)==1)
           c=c+1;
           f = mod((f*base), m);
        end 
        i=i-1;
     end
-    res =f;
+    res=f;
 end
 
