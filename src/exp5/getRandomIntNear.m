ea490cf9425c06c8ad739e86b297d0c31f120ba9
Bruno Ferme Gasparin <bfgasparin@MacBook-Pro-de-Bruno.local>
2014-08-01 04:31:06 -0300

    Created getRamdonIntNear

---
+++ src/exp5/getRandomIntNear.m
@@ -0,0 +1,22 @@
+function [k, q, n] = getRandomIntNear(N)
+	min = ceil(0.9*N);
+	max = ceil(1.1*N);
+	maxk = log2(max);
+	k = randi(maxk);
+	n = (2.^k);
+
+	maxq = ceil((max/n));
+	minq = ceil((min/n));
+	 
+	q = 0;
+	sum = maxq - minq;
+	if sum == 0
+	  sum = 1;
+	end 
+	while mod(q,2)==0
+	  q = minq + randi(sum);
+    end
+	 
+	n = (n * q) + 1;
+
+end
